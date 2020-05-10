88b2ee4d3c0edadb2a66972ee18c1d7ec39c3c94
George Sundarapandian David <437308@cognizant.com>
2020-05-10 13:22:55 -0500

    Added by George

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import logocodesnippet from './logo.svg';
 import './App.css';
-import util from './header';
+import util from './vehicle';
 
-const myfirstelement =     <h1>Hello React!</h1>;
+const myfirstelement = <h1>Hello React!</h1>;
+
 var mycar = new util.Car("Ford");
 const showval = mycar.present();
 var mymodel = new util.model("Ford", "Mustang");
@@ -14,30 +16,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {myfirstelement}
-
-        {showmodel}
+        <table>
+          <tr>
+            <td nowrap>&nbsp;
+              {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            </td>
+            {/* <td>{myfirstelement}</td> */}
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>
+              <img src="logo_codesnippet_01.png" className="App-logosnippet" alt="logo" />
+            </td>
+  <td>{myfirstelement}{showmodel}</td>
+          </tr>
+        </table>
       </header>
     </div>
   );
 }
-
-/* ReactDOM.render(myfirstelement, document.getElementById("root1"));
-ReactDOM.render(<util.Test />, document.getElementById("root2"));
-ReactDOM.render(showval, document.getElementById("root3"));
-ReactDOM.render(showmodel, document.getElementById("root4")); */
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import util from './header';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -14,21 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-/* var mycar = new util.Car("Ford");
-
-const showval = mycar.present();
-
-var mymodel = new util.model("Ford", "Mustang");
-
-const showmodel = mymodel.show();
-
-ReactDOM.render(myfirstelement, document.getElementById("root1"));
-ReactDOM.render(<util.Test />, document.getElementById("root2"));
-ReactDOM.render(showval, document.getElementById("root3"));
-ReactDOM.render(showmodel, document.getElementById("root4")); */
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
